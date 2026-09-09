221fc0ac744fe5c309cfcfdee04327aab9a0faa8
suraz7194-debug <suraz7194@gmail.com>
2026-09-09 12:23:48 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,53 +1,69 @@
-const CACHE_NAME = "daily-tracker-v1";
+const CACHE_NAME = "cee-journey-v2";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
     "./",
     "./index.html",
     "./style.css",
     "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+            .then(cache => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
     );
-
-    self.skipWaiting();
 });
-
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-
+        caches.keys().then(keys => {
             return Promise.all(
-                cacheNames
-                    .filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
             );
-
-        })
+        }).then(() => self.clients.claim())
     );
-
-    self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
 
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
+            .then(cachedResponse => {
 
-                return response || fetch(event.request);
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
 
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (
+                            !response ||
+                            response.status !== 200 ||
+                            response.type !== "basic"
+                        ) {
+                            return response;
+                        }
+
+                        const responseClone = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseClone);
+                            });
+
+                        return response;
+                    })
+                    .catch(() => {
+                        return caches.match("./index.html");
+                    });
             })
     );
-
 });
